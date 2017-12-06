7f1489dda5528cdf051164e70e47ec3149af2543
HoPo-Yi <poyi41@gmail.com>
2017-12-06 20:59:24 +0800

    的

--- poyi.go
+++ poyi.go
@@ -7,7 +7,7 @@ import(
 func poyiReturn(str string) string {
     var result string
     if strings.Contains(str, "G仔"){
-      result = "是poyi哥";
+      if strings.Contains(str, "早安"){result = "poyi哥早安啊"}
     }
     return result
 }
